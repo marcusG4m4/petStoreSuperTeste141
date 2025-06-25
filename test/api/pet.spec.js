0f6351be4a721d01ecff723baf5479ffc3151aef
Marcus Gama <sucramgama@gmail.com>
2025-06-25 15:45:14 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,23 @@
+// Bibliotecas e Frameworks
+const supertest = require('supertest');
+
+const petId = 960107901;
+
+// Em javaScript, Classe é opcional, mas pode agrupar em uma Describe 
+describe('API petStore Swagger - Entidade Pet', () => {
+
+     // Atributos do grupo/describe
+     const request = supertest('https://petstore.swagger.io/v2'); // Base URL da 
+     
+     // Funções ou métodos: its 
+     it('POST Pet', () => {
+          // Atributos, Campos, Caracteristicas, Configurações
+
+     // Funções de Apoio (Opcional)
+
+     // Funções de Teste em si
+               }); // final do metodo post
+
+     })// Funçoes e metodos 
+
+     

fe60e4f481fa26fec4f8fc172eaa6e60d0891f5c
Marcus Gama <sucramgama@gmail.com>
2025-08-05 15:52:41 -0300

    inserindo get

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -1,34 +1,42 @@
 // Bibliotecas e Frameworks
 const supertest = require('supertest');
 
-const petId = 960107901;
+const petId = 960107902;
 
 // Em javaScript, Classe é opcional, mas pode agrupar em uma Describe 
 describe('API petStore Swagger - Entidade Pet', () => {
 
      // Atributos do grupo/describe
-     const request = supertest('https://petstore.swagger.io/v2'); // Base URL da 
-     
-     // Funções ou métodos: its 
-     it('POST Pet', () => {
+     const request = supertest('https://petstore.swagger.io/v2'); // Base URL da
+
+     // Funções ou métodos: its
+     it('POST Pet', async () => {
           // Atributos, Campos, Caracteristicas, Configurações
-         const pet = require('../../vendors/json/pet.json')
-         return request
-              .post('/pet')
-              .send(pet)
-              .then((res) => {
-               expect(res.statusCode).toBe(200)
-               expect(res.body.id).toBe(petId)
-               expect(res.body.name).toBe('Juju')
-               expect(res.body.category.name).toBe('dog')
-               expect(res.body.tags[0].name).toBe('vacinado')
-              }
-          )
-          // Funções de Apoio (Opcional)
+          const pet = await require('../../vendors/json/pet.json')
 
           // Funções de Teste em si
-               }); // final do metodo post
+          return request
+               .post('/pet')
+               .send(pet)
+               .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.id).toBe(petId)
+                    expect(res.body.name).toBe('Juju')
+                    expect(res.body.category.name).toBe('dog')
+                    expect(res.body.tags[0].name).toBe('vacinado')
+               })
 
+
+     }); // final do metodo post
+
+     it('GET Pet', async() => {
+          return await request
+               .get(`/pet/${petId}`)
+               .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.id).toBe(petId)
+                    expect(res.body.status).toBe('available')
+               })
      })// Funçoes e metodos 
 
-     
+})
